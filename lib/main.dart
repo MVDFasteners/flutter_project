c6fa4280f215fef5ad451ede6ab948e2ffbc8279
surya <jayasuraya@mvgroupindia.in>
2025-10-18 16:45:14 +0530

    test commit with surya

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ import 'package:get/get.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:url_strategy/url_strategy.dart';
-
+//  this is testing commit
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   setPathUrlStrategy();
